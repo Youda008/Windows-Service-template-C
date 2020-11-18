be3b1568a4519a8e14e0ee7d25d79e548ac484d7
Youda <youda008@gmail.com>
2020-11-18 21:35:42 +0100

    removed accidentally added file

--- InstallService.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EventLog\Application\TemperatureService" /v "EventMessageFile" /t REG_SZ /d "%~dp0\EventLogMessages.dll"
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EventLog\Application\TemperatureService" /v "TypesSupported" /t REG_DWORD /d 7
-sc create "TemperatureService" binPath= "%~dp0\TemperatureService.exe"
-sc start "TemperatureService"
-pause
