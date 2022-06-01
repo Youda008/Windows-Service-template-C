4e89bf0d3c57f7b756606ba298dc9cb03935d6db
Youda008 <youda008@gmail.com>
2022-06-01 13:54:54 +0200

    fixed forgotten old service name

--- InstallService.bat
+++ InstallService.bat
@@ -2,6 +2,6 @@
 set SERVNAME=MyNewService
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EventLog\Application\%SERVNAME%" /v "EventMessageFile" /t REG_SZ /d "%~dp0\EventLogMessages.dll"
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EventLog\Application\%SERVNAME%" /v "TypesSupported" /t REG_DWORD /d 7
-sc create "%SERVNAME%" binPath= "%~dp0\TemperatureService.exe"
+sc create "%SERVNAME%" binPath= "%~dp0\%SERVNAME%.exe"
 sc start "%SERVNAME%"
 pause
